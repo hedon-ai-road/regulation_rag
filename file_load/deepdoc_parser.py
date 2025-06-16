456f8da9948f1ff7d5538c1cdc1d393d6e2d224b
hedon954 <171725713@qq.com>
2025-06-17 01:49:09 +0800

    file loader, especially use ragflow.deepdoc to load pdf

---
+++ file_load/deepdoc_parser.py
@@ -0,0 +1,15 @@
+from ragflow.rag.app import manual as deepdoc_manual
+
+# should download wordnet first
+# import nltk
+# nltk.set_proxy('http://127.0.0.1:7890')
+# nltk.download('wordnet')
+
+def dummy(prog=None, msg=""):
+    print(prog, msg)
+
+res = deepdoc_manual.chunk(filename="./file_load/fixtures/zhidu_travel.pdf",
+                        callback=dummy)
+for data in res:
+    print("="*100)
+    print(data['content_with_weight'])
